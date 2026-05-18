f61600381d283e1d9f07bbdba40ca4e1153ddfd5
puntig220112 <clemens.puntigam@gmail.com>
2026-05-18 08:51:47 +0200

    cors gemacht

--- BACKEND_NAME_PLACEHOLDER/api/_app.py
+++ BACKEND_NAME_PLACEHOLDER/api/_app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from ._routes import define_routes
 from BACKEND_NAME_PLACEHOLDER.engine import get_engine
@@ -11,6 +12,15 @@ def build_app():
     if not _app:
         get_engine()
         _app = FastAPI()
+        
+        _app.add_middleware(
+            CORSMiddleware,
+            allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+        
         define_routes(_app)
 
     return _app
